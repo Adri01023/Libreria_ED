a282fca62b3a4d8ad6ae6fedc2385d33906993d3
Adri01023 <adri.martin.morales@gmail.com>
2025-01-31 09:19:45 +0100

    Subiendo funcionalidad buscar por autor

--- Book.java
+++ Book.java
@@ -1,7 +1,9 @@
+import java.util.List;
 public class Book {
     private String title;
     private String author;
-
+    private List<Book> books;
+    
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
@@ -15,6 +17,15 @@ public class Book {
         return author;
     }
 
+    public Book findBookByAuthor(String author) {
+        for (Book book : books) {
+            if (book.getAuthor().equalsIgnoreCase(author)) {
+                return book;
+            }
+        }
+        return null;
+    }
+    
     @Override
     public String toString() {
         return "Título: " + title + ", Autor: " + author;
